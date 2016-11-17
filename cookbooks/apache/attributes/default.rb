cbb1b9ee064ecbf6ea1aa98dafaff4b269c47363
root <root@jshaik98572.mylabserver.com>
2016-11-17 23:59:58 +0000

    commit chef repo for the first time

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,2 @@
+default["apache"]["sites"]["jshaik98573"] = { "port" => 80, "domain" => "jshaik98573.mylabserver.com"  }
+default["apache"]["sites"]["jshaik98573b"] = { "port" => 80, "domain" => "jshaik98573b.mylabserver.com" 	}
